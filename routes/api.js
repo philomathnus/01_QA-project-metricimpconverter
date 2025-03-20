3d8a192d43bbe47c0f279c7a23850e5ab4ce66ff
philomathnus <13217349+philomathnus@users.noreply.github.com>
2025-03-20 10:42:59 +0100

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,21 @@
 
 const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
+const { init } = require('../server.js');
 
 module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
+
+  app.get('/api/convert', (req, res) => {
+    const input = req.query.input;
+    const initNum = input.replace(/[^(\d)^(\/)^(\.)]/gi, '');
+    const initUnit = input.replace(/[^a-z]/gi, '');
+    console.log('initNum: ' + initNum);
+    console.log('initUnit: ' + initUnit);
+    const returnedJSON = convertHandler.convert(initNum, initUnit);
+
+    res.json(returnedJSON);
+  });
 
 };
